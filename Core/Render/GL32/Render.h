05b5ffcbc7ac8c555e984bacb600414d4ed73a4b
James Hurley <jamesghurley@gmail.com>
2011-12-18 13:01:00 -0800

    ADD: Dev Root

---
+++ Core/Render/GL32/Render.h
@@ -0,0 +1,105 @@
+
+#ifndef Helios_GL32Render_h
+#define Helios_GL32Render_h
+
+#include <Render/IRender.h>
+#include <Render/ShaderManager.h>
+
+#include <OpenGL/gl3.h>
+#include <OpenGL/gl3ext.h>
+
+namespace helios
+{
+    namespace gl32
+    {
+        struct FBO
+        {
+            GLuint name;
+            GLuint colorBufferTex;
+            GLuint depthBufferTex;
+            GLuint stencilBufferTex;
+        };
+        struct UniformPair
+        {
+            UniformData_ptr uniform;
+            int shader_id;        
+        };
+        class GL32Render : public IRender
+        {
+            
+        protected:
+            
+            ShaderManager mShaderManager;
+            
+            GLuint mDefaultVBO = 0;
+            GLuint mDefaultIBO = 0 ;
+            GLuint mDefaultFBO = 0;
+            GLuint mDiffuseProgram, mShadowVolumeProgram;
+            
+            //std::map<std::string, GLuint> mDiffuseUniforms, mShadowUniforms;
+            
+            std::map<e::RenderStage, FBO> mFBO;
+            std::map<unsigned,RenderGroup> mGroups;
+            
+            std::vector<UniformPair> mUniforms;
+            
+            RenderOptions mOptions;
+            
+            struct
+            {
+                short x, y, w, h;
+            } mCurrentViewport;
+            
+            short mCurrentVBO=0, mCurrentIBO=0, mCurrentVAO=0, mCurrentTex=0, mCurrentShader=0;
+            
+            void GenerateFBO(e::RenderStage, size_t w, size_t h);
+            void RenderStage(e::RenderStage, std::vector<RenderGroup>& gVec);
+            void RenderShadows(std::vector<RenderGroup>& gVec);
+            void SetUniforms(std::vector<UniformData_ptr>& uniformdata, Shader& currentShader, Shader& targetShader, int clear = 0);
+        public:
+            
+            GL32Render();
+            ~GL32Render();
+            unsigned GenerateDefaultVBO();
+            unsigned GenerateDefaultIBO();
+            unsigned int  GenerateVBO(void* data, size_t stride, size_t length) ;
+            unsigned int  GenerateVAO(std::vector<VAOObj> data, unsigned vbo ) ;
+            unsigned int  GenerateIBO(unsigned * indices, size_t size);
+            unsigned int  GenerateIBO(unsigned short * indices, size_t size);
+            
+            void BindVBO ( unsigned int ) ;
+            void BindVAO ( unsigned int ) ;
+            void DeleteVAO ( unsigned int ) ;
+            void DeleteVBO ( unsigned int );
+            void PushRenderCommand ( std::vector<RenderCommand> const & ) ;
+            void ClearViewport(float r = 0., float g = 0., float b = 0., float a = 0.);
+            void Render() ;
+            void Prepare( int fboWidth = 2048, int fboHeight = 2048 );
+            void SetOptions( RenderOptions options ) { mOptions = options ; } ;
+            void ClearRenderStack() ;
+            void SetViewport(int x, int y, int w, int h) ;
+            void SetDefaultFBO( unsigned int fbo )
+            {
+                mDefaultFBO = fbo;
+            };
+            void SetUniform(int shader_name, UniformData_ptr ud);
+            
+            unsigned LoadShader(std::string &vertex_file, std::string &fragment_file, std::map<std::string, int>& attribs, std::map<std::string, int>& uniforms, std::string defines = "");
+            unsigned LoadShader(std::string &vertex_file, std::string &geometry_file, std::string &fragment_file, std::map<std::string, int>& attribs, std::map<std::string, int>& uniforms, std::string defines = "");
+            
+            
+            void SetRenderStage(e::RenderStage stage, std::vector<UniformData> parameters);
+            void ClearRenderStages() {};
+            
+            void DeleteShader(unsigned shader);
+            
+        };
+
+    };
+};
+
+namespace helios
+{
+    using namespace helios::gl32;
+};
+#endif
